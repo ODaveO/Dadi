be654ba66d522d953d82617efb711cd0b7c3e077
Davide_Acquaviva <davacquaviva@gmail.com>
2023-09-22 14:56:26 +0000

    Layout complete

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,39 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Image.asset(
+                'images/dice1.png',
+              ),
+              flex: 1,
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Image.asset(
+                'images/dice6.png',
+              ),
+              flex: 1,
+            ),
+          ]),
+    );
   }
 }
+
+/*
+Image(
+              //margin: EdgeInsets.only(top: 200),
+              image: AssetImage('images/dice1.png'),
+              width: 150,
+            ),
+            SizedBox(width: 20),
+            Image(
+              //margin: EdgeInsets.only(top: 200),
+              image: AssetImage('images/dice6.png'),
+              width: 150,
+            ),
+*/
